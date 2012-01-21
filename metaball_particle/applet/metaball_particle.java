fd5aceb1758a3730e879456ae206cd0da373fbed
Ryohei Suzuki <quolc.i@gmail.com>
2012-01-22 02:33:56 +0900

    modified metaball_particle

--- metaball_particle/applet/metaball_particle.java
+++ metaball_particle/applet/metaball_particle.java
@@ -25,6 +25,8 @@ int mr, mg, mb;
 float simplify = 2;
 int far_threshold = 5000;
 
+boolean stop = false;
+
 public void setup() {
   size(600, 600);
   
@@ -34,12 +36,6 @@ public void setup() {
 
 public void draw() {
   background(0);
-  
-  // calc vx/vy
-  for(int i=0; i<particleSystems.size(); i++) {
-    ParticleSystem ps = (ParticleSystem)this.particleSystems.get(i);
-    ps.calcMetaball();
-  }
   
   // draw metaballs
   pg.beginDraw();
@@ -92,20 +88,6 @@ class ParticleSystem {
       this.particles.add(new Particle(newpos, vel, PApplet.parseInt(random(120, 150))));
     }
   }
-  /*
-  public void drawParticles() {
-    for(int i=0; i<this.particles.size(); i++) {
-      Particle p = ((Particle)this.particles.get(i));
-      p.drawParticle();
-    }
-  }
-  */
-  public void calcMetaball() {
-    for(int i=0; i<this.particles.size(); i++) {
-      Particle p = ((Particle)this.particles.get(i));
-      p.calcMetaball();
-    }
-  }
   
   public void drawMetaball(int x, int y) {
     for(int i=0; i<this.particles.size(); i++) {
@@ -141,17 +123,12 @@ class Particle {
   PVector acc;
   int t;
   int life;
-  int vx[];
-  int vy[];
   float r_ratio = 1, g_ratio = 1, b_ratio = 1;
   
   public Particle(PVector initPos, PVector initVel, int life) {
     this.pos = initPos;
     this.vel = initVel;
     this.life = life;
-    
-    this.vx = new int[pg.width];
-    this.vy = new int[pg.height];
     
     this.acc = new PVector(0, 0.1f);
     
@@ -160,24 +137,9 @@ class Particle {
     this.b_ratio = random(0, 1);
   }
   
-  /*
-  public void drawParticle() {
-    point(this.pos.x, this.pos.y);
-  }
-  */
-  
-  public void calcMetaball() {
-    for(int i=0; i<pg.width; i++) {
-      vx[i] = PApplet.parseInt(sq(this.pos.x - i));
-    }
-    for(int j=0; j<pg.height; j++) {
-      vy[j] = PApplet.parseInt(sq(this.pos.y - j));
-    }
-  }
-  
   public void drawMetaball(int x, int y) {
     if((x-pos.x)*(x-pos.x) + (y-pos.y)*(y-pos.y) > far_threshold) return;
-    float p = 20000/(vy[y] + vx[x] + 1);
+    float p = 20000/((x-pos.x)*(x-pos.x) + (y-pos.y)*(y-pos.y) + 1);
     mr += p * r_ratio;
     mg += p * g_ratio;
     mb += p * b_ratio;
--- metaball_particle/metaball_particle.pde
+++ metaball_particle/metaball_particle.pde
@@ -6,6 +6,8 @@ int mr, mg, mb;
 float simplify = 2;
 int far_threshold = 5000;
 
+boolean stop = false;
+
 void setup() {
   size(600, 600);
   
@@ -15,12 +17,6 @@ void setup() {
 
 void draw() {
   background(0);
-  
-  // calc vx/vy
-  for(int i=0; i<particleSystems.size(); i++) {
-    ParticleSystem ps = (ParticleSystem)this.particleSystems.get(i);
-    ps.calcMetaball();
-  }
   
   // draw metaballs
   pg.beginDraw();
@@ -73,20 +69,6 @@ class ParticleSystem {
       this.particles.add(new Particle(newpos, vel, int(random(120, 150))));
     }
   }
-  /*
-  public void drawParticles() {
-    for(int i=0; i<this.particles.size(); i++) {
-      Particle p = ((Particle)this.particles.get(i));
-      p.drawParticle();
-    }
-  }
-  */
-  public void calcMetaball() {
-    for(int i=0; i<this.particles.size(); i++) {
-      Particle p = ((Particle)this.particles.get(i));
-      p.calcMetaball();
-    }
-  }
   
   public void drawMetaball(int x, int y) {
     for(int i=0; i<this.particles.size(); i++) {
@@ -122,17 +104,12 @@ class Particle {
   PVector acc;
   int t;
   int life;
-  int vx[];
-  int vy[];
   float r_ratio = 1, g_ratio = 1, b_ratio = 1;
   
   public Particle(PVector initPos, PVector initVel, int life) {
     this.pos = initPos;
     this.vel = initVel;
     this.life = life;
-    
-    this.vx = new int[pg.width];
-    this.vy = new int[pg.height];
     
     this.acc = new PVector(0, 0.1);
     
@@ -141,24 +118,9 @@ class Particle {
     this.b_ratio = random(0, 1);
   }
   
-  /*
-  public void drawParticle() {
-    point(this.pos.x, this.pos.y);
-  }
-  */
-  
-  public void calcMetaball() {
-    for(int i=0; i<pg.width; i++) {
-      vx[i] = int(sq(this.pos.x - i));
-    }
-    for(int j=0; j<pg.height; j++) {
-      vy[j] = int(sq(this.pos.y - j));
-    }
-  }
-  
   public void drawMetaball(int x, int y) {
     if((x-pos.x)*(x-pos.x) + (y-pos.y)*(y-pos.y) > far_threshold) return;
-    float p = 20000/(vy[y] + vx[x] + 1);
+    float p = 20000/((x-pos.x)*(x-pos.x) + (y-pos.y)*(y-pos.y) + 1);
     mr += p * r_ratio;
     mg += p * g_ratio;
     mb += p * b_ratio;
